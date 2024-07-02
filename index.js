3dbcc4d5b0406317a4f7f0db4ad37e8103e33fab
Dakshayini <137468494+dakshayini28@users.noreply.github.com>
2024-07-02 16:32:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const display=document.getElementById('display');
+function appendToDisplay(input){
+    display.value+=input;
+}
+function clearDisplay(input){
+    display.value="";
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "Error";
+    }
+}
